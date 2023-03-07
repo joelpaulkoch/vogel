a0219abcd6669575ecb3c58a2471b59dcf536f1f
joel <joel@joelkoch.dev>
2023-03-07 18:49:13 +0100

    add credo as dependency

--- mix.exs
+++ mix.exs
@@ -34,7 +34,8 @@ defmodule Vogel.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ex_doc, "~> 0.27.0", only: :dev, runtime: false}
+      {:ex_doc, "~> 0.27", only: :dev, runtime: false},
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false}
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
   end
